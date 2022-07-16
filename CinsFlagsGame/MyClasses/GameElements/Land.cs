5c13294b55366246a264ed99977c15d5bef484ad
emre-yildiz-dev <emre.yildiz.dev@hotmail.com>
2022-07-16 11:54:10 +0300

    Initalize project

---
+++ CinsFlagsGame/MyClasses/GameElements/Land.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace CinsFlagsGame.MyClasses.GameElements
+{
+    public class Land : Button // Inherited Land has properties from Button.
+    {
+        private Player owner;  // Checking who is the owner of the Land when clicked.
+        private byte coordX;  // assigning one of the 2D Land List index. 
+        private byte coordY;  // assigning the other index.
+
+        public Player Owner { get => owner; set => owner = value; } 
+        public byte CoordX { get => coordX; set => coordX = value; }
+        public byte CoordY { get => coordY; set => coordY = value; }
+    }
+}
---
+++ CinsFlagsGame/MyClasses/GameElements/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace CinsFlagsGame.MyClasses.GameElements
+{
+    public class Player
+    {
+        private string name;
+        private Color flagColor;
+        private int maxOwnLand = 5;  // Players have 5 Land area when the war game starts.
+        private int shootedLand = 0; // this integer is for checking the Final state of the game. 
+        private bool turn;  // Players shoot the correspondence area turn by turn.
+
+        public Player(string name, Color flagColor)
+        {
+            this.name = name;
+            this.flagColor = flagColor;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public Color FlagColor { get => flagColor; set => flagColor = value; }
+        public bool Turn { get => turn; set => turn = value; }
+        public int MaxOwnLand { get => maxOwnLand; set => maxOwnLand = value; }
+        public int ShootedLand { get => shootedLand; set => shootedLand = value; }
+    }
+}
